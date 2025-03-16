aa16cb3c52c32ffc5a59aec9f9c270df51fdca01
Stian Marthinsen <stian.marthinsen@cadworks.no>
2025-03-16 13:06:50 +0100

    prøver å finne ut av komponent ressurser

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -1,5 +1,30 @@
-export default function Resources() {
+import React from "react"
+import resourcesData from '../course_file/ressurser'
+
+export default function Resources({ category }) {
+
+    const filteredResources = resourcesData.filter(
+        (resource) => resource.category === category
+    )
+
     return (
-        <h1>Resources</h1>
+        <section>
+            <h1>Ressurser for {category} </h1>
+            {filteredResources.map((resource, index) => (
+                <article key={index} >
+                    <h2>{resource.category}</h2>
+                    <p>{resource.text}</p>
+                    <ul>
+                        {resource.sources.map((source, i) => (
+                            <li key={i}>
+                                <a href={source.url} target="_blank" rel="noopener noreferrer"> {source.title}</a>
+
+                            </li>
+                        ) ) }
+                    </ul>
+
+                </article>
+            )  )  }
+        </section>
     )
 }
